45088953f3418997753af3e448064d090fe930a6
Sidoryx <siddhantban02@gmail.com>
2022-08-03 21:57:46 +0530

    Div Created with Empty ID to showcase No Matching Results

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -21,6 +21,21 @@ const ItemList = (props) => {
     );
   }
 
+  if (items.length === 0) {
+    return (
+      <div
+        id="empty"
+        className="w-50 mt-5 d-flex flex-column justify-content-center align-items-center empty-container"
+      >
+        <img src={CryingEmoji} alt='Crying Emoji' className="emoji" />
+        <p className="mt-4">
+          No items found for{" "}
+          <span className="highlight-bold">{props.searchTerm}</span>
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="container py-2">
       <div className="row">
