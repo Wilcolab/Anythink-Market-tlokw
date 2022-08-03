4bedd9e01c34e2904a6ab2977997d09f0a78865d
Sidoryx <siddhantban02@gmail.com>
2022-08-03 11:27:58 +0530

    Fixed the backend issue

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -8,7 +8,7 @@ var ItemSchema = new mongoose.Schema(
     slug: { type: String, lowercase: true, unique: true },
     title: String,
     description: String,
-    image: String,
+    image: {type:String,default:'placeholder.png'},
     favoritesCount: { type: Number, default: 0 },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     tagList: [{ type: String }],
