6317c5cf198a18f0b44afb6f4d05e31b77401fa8
Sidoryx <siddhantban02@gmail.com>
2022-08-22 09:34:10 +0530

    Update seeds.js

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,37 +1,71 @@
 const mongoose = require("mongoose");
-const item = require("../models/Item");
-const Item = mongoose.model("Item");
-require('./models/User');
-const User = mongoose.model('User');
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB:", err.message);
-  });
 
-const seedItems = [
-  {
-    slug: "test",
-    title: "test",
-    description: "test",
-    image: "test",
-    favoritesCount: 0,
-    comments: [],
-    tagList: [],
-    seller: {},
-  },
-];
+require("../models/User");
+require("../models/Item");
+require("../models/Comment");
+
+const User = mongoose.model("User");
+const Item = mongoose.model("Item");
+const Comment = mongoose.model("Comment");
+
+if (!process.env.MONGODB_URI) {
+	console.warn("Missing MONGODB_URI in env, please add it to your .env file");
+}
+
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log("\n\nConnection Open\n\n");
+	})
+	.catch((err) => {
+		console.log("\n\n" + err + "\n\n");
+		process.exit();
+	});
 
 const seedDB = async () => {
-  await Item.insertMany(seedItems);
+	//Creating 100 records
+	for (let itr = 0; itr < 100; itr++) {
+		const seedUser = {
+			username: `user${itr}`,
+			email: `user${itr}@gmail.com`,
+			bio: `SDE ${itr}`,
+			image:
+				"https://i1.wp.com/media.globalnews.ca/videostatic/353/499/oldestmanthumb.jpg?w=1040&quality=70&strip=all",
+		};
+
+		const user = new User(seedUser);
+		const userSaved = await user.save();
+
+		const seedItem = {
+			slug: `Product${itr}-${userSaved._id}`,
+			title: `Product${itr}`,
+			description: `User Specialist Product`,
+			image: `https://random.imagecdn.app/360/360`,
+			comments: [],
+			tagList: ["seeding"],
+			seller: userSaved._id,
+		};
+
+		const item = new Item(seedItem);
+		const itemSaved = await item.save();
+
+		const seedComment = {
+			body: "Fake Product, Bad Service",
+			seller: userSaved._id,
+			item: itemSaved._id,
+		};
+
+		const comment = new Comment(seedComment);
+		await comment.save();
+	}
 };
 
-seedDB().then(() => {
-  mongoose.connection.close();
-});
+seedDB()
+	.then(() => {
+		console.log("\n\n\n\nSeeding Successful");
+		process.exit();
+	})
+	.catch((err) => {
+		console.log(err);
+		process.exit();
+	});
